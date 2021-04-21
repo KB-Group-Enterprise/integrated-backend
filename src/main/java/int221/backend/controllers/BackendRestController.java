a021d2263bd286a025cab3e9a2c7b145284e0c06
akira2542 <akiraakamura@gmail.com>
2021-04-22 00:16:55 +0700

    initialize springboot

---
+++ src/main/java/int221/backend/controllers/BackendRestController.java
@@ -0,0 +1,34 @@
+package int221.backend.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@SuppressWarnings("unused")
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+public class BackendRestController {
+	@GetMapping(path="/api/health")
+	public String health() {
+		return "Health is Pretty Good, Thanks for asking!";
+	}
+}
