fb0202bf254dd7793b2ca409589cfb7e8ff8f433
akira2542 <akiraakamura@gmail.com>
2021-04-28 10:02:36 +0700

    [Update] Brand Controller

---
+++ src/main/java/int221/backend/controllers/BrandRestController.java
@@ -0,0 +1,38 @@
+package int221.backend.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import int221.backend.models.entities.Brand;
+import int221.backend.repositories.BrandRepository;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api")
+public class BrandRestController {
+	@Autowired
+	private BrandRepository brandRepository;
+	
+	@GetMapping("/brands")
+    public List<Brand> showAllBrands() {
+            return brandRepository.findAll();
+    }
+	
+	@GetMapping(path="/brands/{id}")
+	public Brand showBrand(@PathVariable long id) {
+		Brand brand = brandRepository.findById(id).orElse(null);
+		return brand;
+	}
+	@PostMapping("/brands")
+	protected Brand Brand(@RequestBody Brand newBrand) {
+		return brandRepository.save(newBrand);
+	} 
+}
