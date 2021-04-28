d5b831efc77a9b63b1a7ed164c21aae822ae8dc5
akira2542 <akiraakamura@gmail.com>
2021-04-28 12:54:27 +0700

    [Update] ImageService Done

--- src/main/java/int221/backend/controllers/PictureRestController.java
+++ src/main/java/int221/backend/controllers/PictureRestController.java
@@ -1,15 +1,13 @@
 package int221.backend.controllers;
 
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.Random;
-
-import org.hibernate.internal.build.AllowSysOut;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,64 +31,33 @@ public class PictureRestController {
             return pictureRepository.findAll();
     }
 
-	private String saveImageFile(@RequestParam("image") MultipartFile imageFile) {
-		String res = "";
-		try {
-			res = this.imageService.save(imageFile);
-		} catch (IOException e) {
-			e.printStackTrace();
-			return e.toString();
-		}
-		return res;
-	}
-
 	@PostMapping("/test/img")
 	private String test(@RequestParam("images") List<MultipartFile> imageFiles) {
 		for ( MultipartFile imageFile : imageFiles) {
 			try {
-				imageService.save(imageFile);
+				imageService.SaveAndInsert(imageFile, 2);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 		return "Ok!";
 	}
-	
-	private Picture savePicture(MultipartFile imageFile,Long carId,String imageName) {
-		Picture newPic = new Picture(0, carId, imageName);
-		return pictureRepository.save(newPic);
-	}
-
-	protected List<Picture> savePictures(MultipartFile[] imageFiles,long carId){
-		for ( MultipartFile imageFile : imageFiles) {
-			this.savePicture(imageFile,carId,this.generateImageName(carId));
+	@GetMapping("/test/img/{id}")
+	private ResponseEntity<byte[]> testGet(@PathVariable("id") long id) {
+		Picture pic = pictureRepository.findById(id).orElse(null);
+		String img_name = pic.getName();
+		MediaType mediaType = imageService.getMediaType(pic.getFiletype());
+		
+		try {
+			byte[] image = imageService.getImageFile(img_name);
+			return ResponseEntity.ok().contentType(mediaType).body(image);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		return null;
-	}
-	
-	private String generateImageName(long carId) {
-		String name = carId + "_" + this.generateRandomString();
-		int limit  = 10;
-		int count = 0;
-		while (!this.pictureRepository.existsByName(name) && count < limit) {
-			name = carId + "_" +generateRandomString();
-			limit++;
-		}
-		return name;
-
-	    
-	}
-	private String generateRandomString() {
-	    int leftLimit = 97;
-	    int rightLimit = 122;
-	    int targetStringLength = 10;
-	    Random random = new Random();
-	    StringBuilder buffer = new StringBuilder(targetStringLength);
-	    for (int i = 0; i < targetStringLength; i++) {
-	        int randomLimitedInt = leftLimit + (int) 
-	          (random.nextFloat() * (rightLimit - leftLimit + 1));
-	        buffer.append((char) randomLimitedInt);
-	    }
-	    return buffer.toString();
+	} 
+	@GetMapping("/test/converter")
+	public String testConverter() {
+		return imageService.getFileTypeFromContentType("image/jpeg");
 	}
 }
