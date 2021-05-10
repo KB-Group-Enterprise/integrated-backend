1a4d909c422e33cb44f9e85385837e89b2e7c6ec
akira2542 <akiraakamura@gmail.com>
2021-05-10 21:54:52 +0700

    [Update] Picture rest controller fix

--- src/main/java/int221/backend/controllers/PictureRestController.java
+++ src/main/java/int221/backend/controllers/PictureRestController.java
@@ -7,15 +7,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.client.HttpServerErrorException;
 
 import int221.backend.models.entities.Picture;
 import int221.backend.models.exception.ImageNotFoundException;
@@ -34,20 +30,6 @@ public class PictureRestController {
     private List<Picture> showAllPictures() {
             return pictureRepository.findAll();
     }
-
-	@PostMapping("/test/img")
-	@ResponseBody
-	private ResponseEntity<Object> test(@RequestParam("images") List<MultipartFile> imageFiles) {
-		for ( MultipartFile imageFile : imageFiles) {
-			try {
-				imageService.SaveAndInsert(imageFile, 2);
-			} catch (IOException e) {
-				e.printStackTrace();
-				return new ResponseEntity<Object>(HttpStatus.INTERNAL_SERVER_ERROR);
-			}
-		}
-		return new ResponseEntity<Object>(HttpStatus.OK);
-	}
 	@GetMapping("/img/{id}")
 	private ResponseEntity<byte[]> getImage(@PathVariable("id") long id) {
 		try {
@@ -59,15 +41,8 @@ public class PictureRestController {
 		} catch (IOException e) {
 			e.printStackTrace();
 			throw new ImageNotFoundException();
-		}
-	}
-	@DeleteMapping("/test/img/delete")
-	private boolean deleteTest() {
-		return imageService.deleteFileThenPicture(10);
-	}
-	
-	@DeleteMapping("/test/img/delete/{carId}")
-	private boolean deleteImagefromCarId(@PathVariable("carId") Long carId) {
-		return imageService.deleteAllPictureByCarId(carId);
+		} catch (Exception e) {
+			throw new HttpServerErrorException(HttpStatus.INTERNAL_SERVER_ERROR);
+		} 
 	}
 }
